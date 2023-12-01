a9e375851ff352cb7b7b7f2b6fd8e5af70b0aba5
Hrushikesh <hrushikeshnikam>
2023-12-01 19:43:20 +0530

    irrigation_updated

---
+++ Projecthon-Team1-Oct23/Code/Irrigation_Sys_Updated.X/build/default/production/Irrigation_System_Oct23.p1.d
@@ -0,0 +1,7 @@
+build/default/production/Irrigation_System_Oct23.p1:  \
+Irrigation_System_Oct23.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/adcc.h  \
+PICSim_Manual_Add_Files/Lcd/LCD_4Bit.h 
---
+++ Projecthon-Team1-Oct23/Code/Irrigation_Sys_Updated.X/build/default/production/PICSim_Manual_Add_Files/Lcd/src/LCD_4Bit.p1.d
@@ -0,0 +1,4 @@
+build/default/production/PICSim_Manual_Add_Files/Lcd/src/LCD_4Bit.p1:  \
+PICSim_Manual_Add_Files/Lcd/src/LCD_4Bit.c  \
+PICSim_Manual_Add_Files/Lcd/src/../LCD_4Bit.h  \
+PICSim_Manual_Add_Files/Lcd/src/../../../mcc_generated_files/device_config.h 
